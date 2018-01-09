e26655eede8396181276a4e60b5f5b2c66eab404
Vitor Cortez <vitor.a.cortez@gmail.com>
2018-01-08 22:30:00 -0300

    Add unit test for Menu activity
    This commit was cherry-picked from a feature branch.

--- app/src/test/java/io/github/vekat/spaceevader/MenuActivityTest.kt
+++ app/src/test/java/io/github/vekat/spaceevader/MenuActivityTest.kt
@@ -1,6 +1,10 @@
 package io.github.vekat.spaceevader
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
 import android.view.MotionEvent
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -8,24 +12,82 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadow.api.Shadow
 import org.robolectric.shadows.ShadowApplication
+import org.robolectric.shadows.ShadowSensorManager
 
 @RunWith(RobolectricTestRunner::class)
 class MenuActivityTest {
     lateinit var activity: MenuActivity
     lateinit var view: MenuView
 
+    lateinit var sensorManager: SensorManager
+    lateinit var shadowSensorManager: ShadowSensorManager
+    lateinit var accelerometer: Sensor
+    lateinit var magnetometer: Sensor
+
     @Before
     fun setup() {
+        val fieldType = Sensor::class.java.getDeclaredField("mType")
+        fieldType.isAccessible = true
+
+        accelerometer = Shadow.newInstanceOf(Sensor::class.java)
+        fieldType.set(accelerometer, Sensor.TYPE_ACCELEROMETER)
+
+        magnetometer = Shadow.newInstanceOf(Sensor::class.java)
+        fieldType.set(magnetometer, Sensor.TYPE_MAGNETIC_FIELD)
+
+        sensorManager = RuntimeEnvironment.application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        shadowSensorManager = shadowOf(sensorManager)
+
+        shadowSensorManager.addSensor(accelerometer.type, accelerometer)
+        shadowSensorManager.addSensor(magnetometer.type, magnetometer)
+
         activity = Robolectric.setupActivity(MenuActivity::class.java)
         view = activity.menuView
     }
 
     @Test
-    fun shouldStartGameActivityOnClick() {
+    fun shouldStartGameActivityWithTouch() {
         val touchEvent = MotionEvent.obtain(200, 300, MotionEvent.ACTION_MOVE, 10f, 10f, 0)
 
         view.dispatchTouchEvent(touchEvent)
+        view.performClick()
+
+        val expectedIntent = Intent(activity, GameActivity::class.java)
+        val actualIntent = ShadowApplication.getInstance().nextStartedActivity
+
+        assertEquals(expectedIntent.component, actualIntent.component)
+    }
+
+    @Test
+    fun shouldStartGameActivityWithSensor() {
+        val accelerometerEvent = shadowSensorManager.createSensorEvent()
+        val magnetometerEvent = shadowSensorManager.createSensorEvent()
+
+        val sensorField = SensorEvent::class.java.getField("sensor")
+        sensorField.isAccessible = true
+
+        sensorField.set(accelerometerEvent, accelerometer)
+        sensorField.set(magnetometerEvent, magnetometer)
+
+        val valuesField = SensorEvent::class.java.getField("values")
+        valuesField.isAccessible = true
+
+        valuesField.set(accelerometerEvent, floatArrayOf(0f, 0f, 10f))
+        valuesField.set(magnetometerEvent, floatArrayOf(50f, 50f, 0f))
+
+        val beforeEvents = view.xRelativeToWidth
+        println(beforeEvents)
+
+        view.onSensorChanged(accelerometerEvent)
+        view.onSensorChanged(magnetometerEvent)
+
+        println(view.xRelativeToWidth)
+        assertEquals("Should be different",true, beforeEvents != view.xRelativeToWidth)
+
         view.performClick()
 
         val expectedIntent = Intent(activity, GameActivity::class.java)
